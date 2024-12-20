5c12e183e5d87d6ae7fe375d125b6aaccfa109d1
Sudhanshu Gautam <2022csb1130@iitrpr.ac.in>
2024-12-20 16:05:36 +0530

    Backend Done !!

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-
+import axios from "axios";
+import toast from "react-hot-toast";
 function Login() {
   const {
     register,
@@ -9,8 +10,31 @@ function Login() {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data); // You can handle form data here
+  const onSubmit = async (data) => {
+    const userInfo = {
+      email: data.email,
+      password: data.password,
+    };
+    await axios
+      .post("http://localhost:4001/user/login", userInfo)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data) {
+          toast.success('Logined in Successfully');
+          document.getElementById("my_modal_3").close();
+          setTimeout(() =>{
+            window.location.reload();
+            localStorage.setItem("Users",JSON.stringify(res.data.user));
+          },1000);
+        }
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err);
+          toast.error("error: " + err.response.data.message);
+          setTimeout(()=>{},1000);
+        }
+      }); // You can handle form data here
   };
 
   return (
